06282eff46cf480e920aba3a6767f0a2c4b21c10
chobyounghak <61299886+chobyounghak@users.noreply.github.com>
2020-09-24 18:20:05 +0900

    Dd

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -382,8 +382,8 @@ class PathPlanner():
           self.angle_steers_des_mpc = self.limit_ctrl( org_angle_steers_des, limit_steers, angle_steers )
 
     elif v_ego_kph < 40:  # 30
-      xp = [10,20,30]
-      fp2 = [1,3,5]
+      xp = [10,20,30,40]
+      fp2 = [1,3,5,7]
       limit_steers = interp( v_ego_kph, xp, fp2 )
       self.angle_steers_des_mpc = self.limit_ctrl( org_angle_steers_des, limit_steers, angle_steers )
     elif v_ego_kph > 60: 
