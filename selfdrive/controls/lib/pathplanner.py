9a8ddb647540e4b067329660eadda75b0e8d3013
chobyounghak <61299886+chobyounghak@users.noreply.github.com>
2020-09-30 19:12:04 +0900

    dd

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -25,7 +25,7 @@ LOG_MPC = os.environ.get('LOG_MPC', True)
 
 LANE_CHANGE_SPEED_MIN = 40 * CV.KPH_TO_MS
 LANE_CHANGE_TIME_MAX = 10.
-DST_ANGLE_LIMIT = 8
+DST_ANGLE_LIMIT = 8.
 
 DESIRES = {
   LaneChangeDirection.none: {
