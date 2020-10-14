3a66b93bbcd190436dba157860122820e74758f6
romancer <36905757+leejeonggoo@users.noreply.github.com>
2020-10-14 22:53:48 +0900

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -282,14 +282,14 @@ class CarController():
     # send mdps12 to LKAS to prevent LKAS error if no cancel cmd
     can_sends.append( create_mdps12(self.packer, frame, CS.mdps12) )
 
-    str_log1 = 'torg:{:5.0f}/{:5.0f}/{:5.0f}  CV={:5.1f}/{:5.1f}'.format(  apply_steer, new_steer, dst_steer, self.model_speed, self.model_sum  )
-    str_log2 = 'limit={:.0f} tm={:.1f} '.format( apply_steer_limit, self.timer1.sampleTime()  )
-    trace1.printf( '{} {}'.format( str_log1, str_log2 ) )
+    #str_log1 = 'torg:{:5.0f}/{:5.0f}/{:5.0f}  CV={:5.1f}/{:5.1f}'.format(  apply_steer, new_steer, dst_steer, self.model_speed, self.model_sum  )
+    #str_log2 = 'limit={:.0f} tm={:.1f} '.format( apply_steer_limit, self.timer1.sampleTime()  )
+    #trace1.printf( '{} {}'.format( str_log1, str_log2 ) )
 
     run_speed_ctrl = self.param_OpkrAccelProfile and CS.acc_active and self.SC != None
     if not run_speed_ctrl:
       str_log2 = 'LKAS={:.0f}  steer={:5.0f} '.format(  CS.lkas_button_on,  CS.out.steeringTorque  )
-      trace1.printf2( '{}'.format( str_log2 ) )
+      #trace1.printf2( '{}'.format( str_log2 ) )
 
     if pcm_cancel_cmd and self.CP.longcontrolEnabled:
       can_sends.append( create_clu11(self.packer, frame, CS.clu11, Buttons.CANCEL) )
